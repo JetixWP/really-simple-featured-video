5b3170eeffa56168123ed728cacc0f32474470a2
Krishna Kant <lushkant@gmail.com>
2021-01-10 21:56:41 +0530

    Add fremius integration

--- really-simple-featured-video.php
+++ really-simple-featured-video.php
@@ -20,6 +20,42 @@ define( 'RSFV_PLUGIN_URL', plugin_dir_url( RSFV_PLUGIN_FILE ) );
 define( 'RSFV_PLUGIN_DIR', plugin_dir_path( RSFV_PLUGIN_FILE ) );
 define( 'RSFV_PLUGIN_BASE', plugin_basename( RSFV_PLUGIN_FILE ) );
 
+if ( ! function_exists( 'rsfv_fs' ) ) {
+	// Create a helper function for easy SDK access.
+	function rsfv_fs() {
+		global $rsfv_fs;
+
+		if ( ! isset( $rsfv_fs ) ) {
+			// Include Freemius SDK.
+			require_once dirname(__FILE__) . '/freemius/start.php';
+
+			$rsfv_fs = fs_dynamic_init( array(
+				'id'                  => '7560',
+				'slug'                => 'really-simple-featured-video',
+				'type'                => 'plugin',
+				'public_key'          => 'pk_6d1ecdde5701fc2158193cf7eab45',
+				'is_premium'          => false,
+				'has_addons'          => false,
+				'has_paid_plans'      => false,
+				'menu'                => array(
+					'slug'           => 'rsfv-settings',
+					'support'        => false,
+					'parent'         => array(
+						'slug' => 'options-general.php',
+					),
+				),
+			) );
+		}
+
+		return $rsfv_fs;
+	}
+
+	// Init Freemius.
+	rsfv_fs();
+	// Signal that SDK was initiated.
+	do_action( 'rsfv_fs_loaded' );
+}
+
 /**
  * Fire up plugin instance.
  */
