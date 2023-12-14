ba5b4066ae3149641bbaea559751f299a7958424
Krishna Kant Chourasiya <lushkant@gmail.com>
2023-12-14 18:49:09 +0530

    Add support for Twenty Twenty four theme

---
+++ includes/Compatibility/Themes/Core/Twentytwenty_Four/class-compatibility.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Twenty Twenty Four theme compatibility handler.
+ *
+ * @package RSFV
+ */
+
+namespace RSFV\Compatibility\Themes\Core\Twentytwenty_Four;
+
+use RSFV\Compatibility\Themes\Base_Compatibility;
+use RSFV\Options;
+
+/**
+ * Class Compatibility
+ *
+ * @package RSFV
+ */
+class Compatibility extends Base_Compatibility {
+	/**
+	 * Class instance.
+	 *
+	 * @var $instance
+	 */
+	protected static $instance;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $id Compat ID.
+	 * @param string $title Compat title.
+	 */
+	public function __construct( $id, $title ) {
+		parent::__construct( $id, $title );
+
+		$this->setup();
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+		if ( has_action( 'rsfv_woo_archives_product_thumbnails', 'woocommerce_template_loop_product_thumbnail' ) ) {
+			remove_action( 'rsfv_woo_archives_product_thumbnails', 'woocommerce_template_loop_product_thumbnail', 10 );
+		}
+	}
+
+	/**
+	 * Enqueue scripts.
+	 *
+	 * @return void
+	 */
+	public function enqueue_scripts() {
+		// Register styles.
+		wp_register_style( 'rsfv-twentytwentyfour', $this->get_current_dir_url() . 'Core/Twentytwenty_Four/styles.css', array(), filemtime( $this->get_current_dir() . 'Core/Twentytwenty_Four/styles.css' ) );
+
+		// Enqueue styles.
+		wp_enqueue_style( 'rsfv-twentytwentyfour' );
+	}
+
+	/**
+	 * Setup compat.
+	 *
+	 * @return void
+	 */
+	public function setup() {
+		// Removes old support for Woo FSE archives.
+		$this->remove_woo_fse_archives_support();
+	}
+
+	/**
+	 * Removes Woo Archives support for fse themes.
+	 *
+	 * @return void
+	 */
+	public function remove_woo_fse_archives_support() {
+		if ( ! class_exists( '\WooCommerce' ) ) {
+			return;
+		}
+
+		$options = Options::get_instance();
+
+		$woo_archives_supported = $options->get( 'woo_archives_supported', true );
+
+		if ( $woo_archives_supported ) {
+			$options->set( 'woo_archives_supported', false );
+		}
+	}
+}
