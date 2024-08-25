b056bccc99080e721240e1d74fdb274d7877ff61
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2024-08-25 14:45:56 +0530

    Update Get Pro tab

--- includes/Settings/Views/html-admin-settings-getpro.php
+++ includes/Settings/Views/html-admin-settings-getpro.php
@@ -14,12 +14,14 @@ namespace RSFV\Settings\views;
 	<h1 class="tab-heading"><?php esc_html_e( 'Get Premium Featured Video support with Really Simple Featured Video Pro', 'rsfv' ); ?></h1>
 	<p>Loving the free version of RSFV? We have our PRO Plugin available now with a suite of features you will love even more, give it a try and optimise your Featured Video workflow with these useful features:</p>
 	<ul>
+		<li><?php esc_html_e( 'Priority Support', 'rsfv' ); ?></li>
 		<li><?php esc_html_e( 'Change Video Aspect Ratio', 'rsfv' ); ?></li>
 		<li><?php esc_html_e( 'Change Video Order at WooCommerce Product page', 'rsfv' ); ?></li>
-		<li><?php esc_html_e( 'Priority Support', 'rsfv' ); ?></li>
+		<li><?php esc_html_e( 'Change Default Gallery Video Thumbnail at Woo Single Product page', 'rsfv' ); ?></li>
+		<li><?php esc_html_e( 'Support for more Premium/Custom themes', 'rsfv' ); ?></li>
 		<li><?php esc_html_e( 'Requests for Theme Compatibility', 'rsfv' ); ?></li>
 	</ul>
 	<p><?php esc_html_e( '.. and more coming soon.', 'rsfv' ); ?></p>
-	<h1 class="offer-highlight">Grab the Premium version starting at just <strong>$39.99</strong>.</h1>
+	<h1 class="offer-highlight">Grab the Premium version starting at just <strong>$39</strong>!</h1>
 	<a href="https://jetixwp.com/plugins/really-simple-featured-video?utm_campaign=settings-protab&utm_source=rsfv-plugin" target="_blank" class="rsfv-button button-primary"><?php esc_html_e( 'Explore RSFV and PRO', 'rsfv' ); ?></a>
 </div>
