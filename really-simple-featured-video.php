daf99ea07af40ad888953c648585c5a6738ed3e1
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-01-31 23:35:02 +0530

    Bump version and add changelog

--- really-simple-featured-video.php
+++ really-simple-featured-video.php
@@ -3,7 +3,7 @@
  * Plugin Name: Really Simple Featured Video
  * Plugin URI:  https://jetixwp.com/plugins/really-simple-featured-video
  * Description: Adds support for Featured Video to WordPress posts, pages & WooCommerce products.
- * Version:     0.22.0
+ * Version:     0.23.0
  * Author:      JetixWP Plugins
  * Author URI:  https://jetixwp.com
  * License:     GPL2
@@ -18,7 +18,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'RSFV_VERSION', '0.22.0' );
+define( 'RSFV_VERSION', '0.23.0' );
 define( 'RSFV_PLUGIN_FILE', __FILE__ );
 define( 'RSFV_PLUGIN_URL', plugin_dir_url( RSFV_PLUGIN_FILE ) );
 define( 'RSFV_PLUGIN_DIR', plugin_dir_path( RSFV_PLUGIN_FILE ) );
