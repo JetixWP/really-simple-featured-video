5f9b5b744e09c9e27f6077b3d1492bed59dfa9ec
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-05-30 13:10:48 +0530

    Add Bricks support

---
+++ includes/Compatibility/Plugins/Bricks/class-compatibility.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Bricks's compatibility handler.
+ *
+ * @package RSFV
+ */
+
+namespace RSFV\Compatibility\Plugins\Bricks;
+
+defined( 'ABSPATH' ) || exit;
+
+use RSFV\Compatibility\Plugins\Base_Compatibility;
+use RSFV\FrontEnd;
+use RSFV\Options;
+
+/**
+ * Class Compatibility
+ *
+ * @package RSFV
+ */
+class Compatibility extends Base_Compatibility {
+	/**
+	 * Class instance.
+	 *
+	 * @var $instance
+	 */
+	protected static $instance;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		$this->id = 'bricks';
+
+		$this->setup();
+	}
+
+	/**
+	 * Register Settings.
+	 *
+	 * @param array $settings Active settings file array.
+	 *
+	 * @return array
+	 */
+	public function register_settings( $settings ) {
+		// Settings.
+		$settings[] = include 'class-settings.php';
+
+		return $settings;
+	}
+
+	/**
+	 * Sets up hooks and filters.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function setup() {
+		add_action(
+			'after_setup_theme',
+			function() {
+				add_action(
+					'init',
+					function() {
+						if ( ! class_exists( 'Bricks\Element' ) ) {
+							return;
+						}
+
+						$element_files = array(
+							__DIR__ . '/elements/really-simple-featured-video.php',
+						);
+
+						foreach ( $element_files as $file ) {
+							\Bricks\Elements::register_element( $file );
+						}
+					}
+				);
+			}
+		);
+	}
+}
