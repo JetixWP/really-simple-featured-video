b3670d57967e7e2172c0d093b121e477122af89a
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-09-11 19:53:06 +0530

    Prepare controls for extension

--- includes/Settings/Tabs/class-controls.php
+++ includes/Settings/Tabs/class-controls.php
@@ -25,6 +25,18 @@ class Controls extends Settings_Page {
 	}
 
 	/**
+	 * Get sections.
+	 *
+	 * @return array
+	 */
+	public function get_sections() {
+		$sections = array(
+			'' => __( 'Standard', 'rsfv' ),
+		);
+		return apply_filters( 'rsfv_get_sections_' . $this->id, $sections );
+	}
+
+	/**
 	 * Get settings array.
 	 *
 	 * @param string $current_section Current section ID.
@@ -32,69 +44,75 @@ class Controls extends Settings_Page {
 	 * @return array
 	 */
 	public function get_settings( $current_section = '' ) {
-		$autoplay_note = __( 'Note: Autoplay will only work if mute sound is enabled as per browser policy.', 'rsfv' );
+		global $current_section;
 
-		$control_options = array(
-			'controls' => __( 'Controls', 'rsfv' ),
-			'autoplay' => __( 'Autoplay', 'rsfv' ),
-			'loop'     => __( 'Loop', 'rsfv' ),
-			'pip'      => __( 'Picture in Picture', 'rsfv' ),
-			'mute'     => __( 'Mute sound', 'rsfv' ),
-		);
+		$settings = array();
 
-		$default_controls = get_default_video_controls();
+		if ( '' === $current_section ) {
+			$autoplay_note = __( 'Note: Autoplay will only work if mute sound is enabled as per browser policy.', 'rsfv' );
 
-		$settings = apply_filters(
-			'rsfv_controls_settings',
-			array(
+			$control_options = array(
+				'controls' => __( 'Controls', 'rsfv' ),
+				'autoplay' => __( 'Autoplay', 'rsfv' ),
+				'loop'     => __( 'Loop', 'rsfv' ),
+				'pip'      => __( 'Picture in Picture', 'rsfv' ),
+				'mute'     => __( 'Mute sound', 'rsfv' ),
+			);
+
+			$default_controls = get_default_video_controls();
+
+			$settings = apply_filters(
+				'rsfv_controls_settings',
 				array(
-					'title' => esc_html_x( 'Self-hosted videos', 'settings title', 'rsfv' ),
-					'desc'  => __( 'Please select the controls you wish to enable for your self hosted videos.', 'rsfv' ),
-					'class' => 'rsfv-self-video-controls',
-					'type'  => 'content',
-					'id'    => 'rsfv-self-video-controls',
-				),
-				array(
-					'type' => 'title',
-					'id'   => 'rsfv_self_video_controls_title',
-				),
-				array(
-					'title'   => '',
-					'desc'    => $autoplay_note,
-					'id'      => 'self_video_controls',
-					'default' => $default_controls,
-					'type'    => 'multi-checkbox',
-					'options' => $control_options,
-				),
-				array(
-					'type' => 'sectionend',
-					'id'   => 'rsfv_self_video_controls_title',
-				),
-				array(
-					'title' => esc_html_x( 'Embed videos', 'settings title', 'rsfv' ),
-					'desc'  => __( 'Please select the controls you wish to enable for your embedded videos.', 'rsfv' ),
-					'class' => 'rsfv-embed-video-controls',
-					'type'  => 'content',
-					'id'    => 'rsfv-embed-video-controls',
-				),
-				array(
-					'type' => 'title',
-					'id'   => 'rsfv_self_embed_controls_title',
-				),
-				array(
-					'title'   => '',
-					'desc'    => $autoplay_note,
-					'id'      => 'embed_video_controls',
-					'default' => $default_controls,
-					'type'    => 'multi-checkbox',
-					'options' => $control_options,
-				),
-				array(
-					'type' => 'sectionend',
-					'id'   => 'rsfv_embed_video_controls_title',
-				),
-			)
-		);
+					array(
+						'title' => esc_html_x( 'Self-hosted videos', 'settings title', 'rsfv' ),
+						'desc'  => __( 'Please select the controls you wish to enable for your self hosted videos.', 'rsfv' ),
+						'class' => 'rsfv-self-video-controls',
+						'type'  => 'content',
+						'id'    => 'rsfv-self-video-controls',
+					),
+					array(
+						'type' => 'title',
+						'id'   => 'rsfv_self_video_controls_title',
+					),
+					array(
+						'title'   => '',
+						'desc'    => $autoplay_note,
+						'id'      => 'self_video_controls',
+						'default' => $default_controls,
+						'type'    => 'multi-checkbox',
+						'options' => $control_options,
+					),
+					array(
+						'type' => 'sectionend',
+						'id'   => 'rsfv_self_video_controls_title',
+					),
+					array(
+						'title' => esc_html_x( 'Embed videos', 'settings title', 'rsfv' ),
+						'desc'  => __( 'Please select the controls you wish to enable for your embedded videos.', 'rsfv' ),
+						'class' => 'rsfv-embed-video-controls',
+						'type'  => 'content',
+						'id'    => 'rsfv-embed-video-controls',
+					),
+					array(
+						'type' => 'title',
+						'id'   => 'rsfv_self_embed_controls_title',
+					),
+					array(
+						'title'   => '',
+						'desc'    => $autoplay_note,
+						'id'      => 'embed_video_controls',
+						'default' => $default_controls,
+						'type'    => 'multi-checkbox',
+						'options' => $control_options,
+					),
+					array(
+						'type' => 'sectionend',
+						'id'   => 'rsfv_embed_video_controls_title',
+					),
+				)
+			);
+		}
 
 		return apply_filters( 'rsfv_get_settings_' . $this->id, $settings );
 	}
