c4858fcda3aec25f5597168af7bfa408d9deee58
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-09-14 01:14:16 +0530

    Add promo controls at Autoplay on Hover settngs

--- includes/Settings/Tabs/class-controls.php
+++ includes/Settings/Tabs/class-controls.php
@@ -9,6 +9,8 @@ namespace RSFV\Settings;
 
 defined( 'ABSPATH' ) || exit;
 
+use RSFV\Plugin;
+
 /**
  * Video frame controls.
  */
@@ -114,65 +116,155 @@ class Controls extends Settings_Page {
 				)
 			);
 		} else if ( 'hover-autoplay' === $current_section ) {
+
+			$settings = array(
+				array(
+					'title' => __( 'Hover Autoplay Controls', 'rsfv' ),
+					'desc'  => sprintf(
+						'%1$s',
+						__( 'Below you can manage the visibility of each Style Kits panel individually. Any existing values in a disabled Style Kit panel will lose its values.', 'rsfv' ),
+					),
+					'type'  => 'content',
+					'id'    => 'rsfv-pro-hover-autoplay-controls',
+				),
+				array(
+					'type' => 'title',
+					'id'   => 'rsfv_pro_hover_autoplay',
+				),
+				array(
+					'title'   => __( 'Enable BETA Feature', 'rsfv' ),
+					'id'      => 'enable_hover_autoplay',
+					'default' => false,
+					'type'    => 'checkbox',
+				),
+				array(
+					'type' => 'sectionend',
+					'id'   => 'rsfv_pro_hover_autoplay',
+				),
+				array(
+					'title' => esc_html_x( 'Video Types', 'settings title', 'rsfv' ),
+					'desc'  => __( 'Please toggle the video types you wish to enable/disable autoplay on hover support at.', 'rsfv' ),
+					'type'  => 'content',
+					'class' => 'rsfv-multi-checkbox-card',
+					'id'    => 'rsfv-pro-hover-autoplay-video-types',
+				),
+				array(
+					'type' => 'title',
+					'id'   => 'rsfv_pro_hover_autoplay_video_types_title',
+				),
+				array(
+					'title'   => '',
+					'id'      => 'hover_autoplay_video_types',
+					'default' => array(
+						'html5' => true,
+						'youtube'  => true,
+						'vimeo'  => true,
+						'dailymotion'  => true,
+					),
+					'type'    => 'multi-checkbox',
+					'options' => array(
+						'html5'      => __( 'Self Hosted', 'rsfv' ),
+						'youtube'    => __( 'YouTube', 'rsfv' ),
+						'vimeo'      => __( 'Vimeo', 'rsfv' ),
+						'dailymotion' => __( 'Dailymotion', 'rsfv' ),
+					),
+				),
+				array(
+					'type' => 'sectionend',
+					'id'   => 'rsfv_pro_hover_autoplay_video_types_title',
+				),
+			);
+
+			if ( ! Plugin::get_instance()->has_pro_active() ) {
+				$settings = array_merge(
+					$settings,
+					array(
+						array(
+							'title' => esc_html_x( 'Screen Sizes', 'settings title', 'rsfv' ),
+							'desc'  => __( 'Toggle the screen sizes you wish to enable/disable autoplay on hover support. Available in Pro.', 'rsfv' ),
+							'type'  => 'promo-content',
+							'class' => 'rsfv-promo-multi-checkbox-card',
+							'id'    => 'promo-rsfv-pro-hover-autoplay-screens',
+						),
+						array(
+							'type' => 'title',
+							'id'   => 'promo_hover_autoplay_screens_title',
+						),
+						array(
+							'title'   => '',
+							'id'      => 'promo-hover-autoplay-screens',
+							'default' => array(
+								'desktop' => true,
+								'mobile'  => true,
+							),
+							'type'    => 'promo-multi-checkbox',
+							'options' => array(
+								'desktop' => __( 'Desktop', 'rsfv' ),
+								'mobile'  => __( 'Mobile', 'rsfv' ),
+							),
+						),
+						array(
+							'type' => 'sectionend',
+							'id'   => 'promo_hover_autoplay_screens_title',
+						),
+						array(
+							'type' => 'title',
+							'id'   => 'promo_hover_autoplay_extras',
+						),
+						array(
+							'title'   => __( 'Set Mobile Breakpoint (px)', 'rsfv' ),
+							'desc'    => __( 'Screen width below which device is considered mobile. Default: 768px', 'rsfv' ),
+							'id'      => 'promo-hover-autoplay-mobile-breakpoint',
+							'default' => 768,
+							'type'    => 'promo-number',
+						),
+						array(
+							'title'   => __( 'Set Hover Delay (ms)', 'rsfv' ),
+							'desc'      => __( 'Delay before video starts playing on hover. Default: 200ms', 'rsfv' ),
+							'id'      => 'promo-hover-autoplay-delay',
+							'default' => 100,
+							'type'    => 'promo-number',
+						),
+						array(
+							'type' => 'sectionend',
+							'id'   => 'promo_hover_autoplay_extras',
+						),
+						array(
+							'title' => __( 'Accessibility', 'rsfv' ),
+							'desc'  => __( 'Toggle the accessibility features at autoplay on hover. Available in Pro.', 'rsfv' ),
+							'type'  => 'promo-content',
+							'id'    => 'promo-hover-autoplay-accessibility',
+							'class' => 'promo-hover-autoplay-accessibility',
+						),
+						array(
+							'type' => 'title',
+							'id'   => 'promo_hover_autoplay_extras',
+						),
+						array(
+							'title'   => __( 'User Preferences', 'rsfv' ),
+							'desc'    => __( 'Respect "reduced motion" preference', 'rsfv' ),
+							'id'      => 'promo-hover-autoplay-respect-user-prefs',
+							'default' => true,
+							'type'    => 'promo-checkbox',
+						),
+						array(
+							'title'   => __( 'Focus Events', 'rsfv' ),
+							'desc'    => __( 'Enable focus events for keyboard navigation', 'rsfv' ),
+							'id'      => 'promo-hover-autoplay-focus-events',
+							'default' => true,
+							'type'    => 'promo-checkbox',
+						),
+						array(
+							'type' => 'sectionend',
+							'id'   => 'promo_hover_autoplay_accessibility',
+						),
+					)
+				);
+			}
+
 			$settings = apply_filters(
 				'rsfv_controls_hover_autoplay_settings',
-				array(
-					array(
-						'title' => __( 'Hover Autoplay Controls', 'rsfv' ),
-						'desc'  => sprintf(
-							'%1$s',
-							__( 'Below you can manage the visibility of each Style Kits panel individually. Any existing values in a disabled Style Kit panel will lose its values.', 'rsfv' ),
-						),
-						'type'  => 'content',
-						'id'    => 'rsfv-pro-hover-autoplay-controls',
-					),
-					array(
-						'type' => 'title',
-						'id'   => 'rsfv_pro_hover_autoplay',
-					),
-					array(
-						'title'   => __( 'Enable BETA Feature', 'rsfv' ),
-						'id'      => 'enable_hover_autoplay',
-						'default' => false,
-						'type'    => 'checkbox',
-					),
-					array(
-						'type' => 'sectionend',
-						'id'   => 'rsfv_pro_hover_autoplay',
-					),
-					array(
-						'title' => esc_html_x( 'Video Types', 'settings title', 'rsfv' ),
-						'desc'  => __( 'Please toggle the video types you wish to enable/disable autoplay on hover support at.', 'rsfv' ),
-						'type'  => 'content',
-						'class' => 'rsfv-multi-checkbox-card',
-						'id'    => 'rsfv-pro-hover-autoplay-video-types',
-					),
-					array(
-						'type' => 'title',
-						'id'   => 'rsfv_pro_hover_autoplay_video_types_title',
-					),
-					array(
-						'title'   => '',
-						'id'      => 'hover_autoplay_video_types',
-						'default' => array(
-							'html5' => true,
-							'youtube'  => true,
-							'vimeo'  => true,
-							'dailymotion'  => true,
-						),
-						'type'    => 'multi-checkbox',
-						'options' => array(
-							'html5'      => __( 'Self Hosted', 'rsfv' ),
-							'youtube'    => __( 'YouTube', 'rsfv' ),
-							'vimeo'      => __( 'Vimeo', 'rsfv' ),
-							'dailymotion' => __( 'Dailymotion', 'rsfv' ),
-						),
-					),
-					array(
-						'type' => 'sectionend',
-						'id'   => 'rsfv_pro_hover_autoplay_video_types_title',
-					),
-				)
+				$settings
 			);
 		}
 
