39c2a97f6387224b470e6c80e34ea28ab603c80d
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-12-10 10:50:42 +0530

    Update composer vendor

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'jetixwp/really-simple-featured-video',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '74722a573d05f09111de5f05faea5e9dc6530212',
+        'reference' => '05b6d66e5f6658623f0d57fbce9697b7e7652632',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'jetixwp/really-simple-featured-video' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '74722a573d05f09111de5f05faea5e9dc6530212',
+            'reference' => '05b6d66e5f6658623f0d57fbce9697b7e7652632',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
