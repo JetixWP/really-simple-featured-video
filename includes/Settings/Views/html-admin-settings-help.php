f2c8d0924c2802112c82c107f34be6c7d72cc834
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-12-29 00:11:47 +0530

    Update settings styles and help tab

--- includes/Settings/Views/html-admin-settings-help.php
+++ includes/Settings/Views/html-admin-settings-help.php
@@ -12,7 +12,7 @@ namespace RSFV\Settings\views;
 
 <div class="help-content">
 	<h1 class="tab-heading"><?php esc_html_e( 'Help and Support', 'rsfv' ); ?></h1>
-	<p>As this is a free plugin and us being a really small team, we are unable to provide support.</p>
+	<p>As this is a free plugin and us being a really small team, we are unable to provide support for all the requests.</p>
 	<p>You may ask the WordPress community for help by posting to the <a href="https://wordpress.org/support/plugin/really-simple-featured-video/" target="_blank">WordPress.org support forum</a>. Response time can range from a few days to a few weeks and will likely be from a non-developer.</p>
 
 	<p>If you want a timely response via email from a developer who works on this plugin, upgrade to <a href="https://jetixwp.com/plugins/really-simple-featured-video?utm_campaign=settings-helptab&utm_source=rsfv-plugin" target="_blank">RSFV Pro</a> and send us an email via the account page at <a  href="https://jetixwp.com/plugins" target="_blank">JetixWP.com</a>.</p>
