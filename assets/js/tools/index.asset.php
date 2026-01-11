64a27482bc4b31307d694dfacafc4229663e99c5
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2026-01-11 17:23:44 +0530

    Bring settings header at tools

--- assets/js/tools/index.asset.php
+++ assets/js/tools/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9faff645331c272258ed');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '43dc3026fac33f57e917');
--- includes/Tools/Views/html-admin-tools.php
+++ includes/Tools/Views/html-admin-tools.php
@@ -13,7 +13,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <div class="wrap rsfv rsfv-tools rsfv-tools-page">
-	<h2><?php esc_html_e( 'RS Featured Video → Tools', 'rsfv' ); ?></h2>
-	<p><?php esc_html_e( 'Manage your featured videos using the powerful tools below.', 'rsfv' ); ?></p>
+	<div class="plugin-header">
+		<div class="plugin-header-wrap">
+			<div class="plugin-info">
+				<h1 class="menu-title"><?php esc_html_e( 'Really Simple Featured Video → Tools', 'rsfv' ); ?></h1>
+				<?php do_action( 'rsfv_extend_plugin_header' ); ?>
+			</div>
+
+			<div class="brand-info">
+				<a href="https://jetixwp.com?utm_campaign=settings-header&utm_source=rsfv-plugin" target="_blank"><img class="brand-logo" src="<?php echo esc_url( RSFV_PLUGIN_URL . 'assets/images/jwp-icon-dark.svg' ); ?>" alt="RSFV"></a>
+			</div>
+		</div>
+	</div>
 	<div id="rsfv-tools-app"></div>
 </div>
